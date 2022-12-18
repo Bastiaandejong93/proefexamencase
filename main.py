06c939c63d430f5b0780f881d02c90cbcb3bc8fb
Bastiaandejong93 <116293329+Bastiaandejong93@users.noreply.github.com>
2022-12-18 14:13:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-#Opties=['Kleine friet 2.20','Middel friet 2.50','Grote friet 3.00']
-#Extras=['Speciaal+ 1','Stoofvleessaus + 1.8','Geen extras']
-#Saus=['Op de frieten of Klein bakje + 0.5','Groot bakje + 0.8','Geen saus']
-#Snacks=['Hamburger(Frikandel) Boulet Mexicano Servola 2.30','Kipnuggets Fingers 2.50','Bicky burger 3,20','Geen snack']
-
 Opties=('Kleine friet','Middel friet','Grote friet')
 Extras=['Speciaal','Stoofvleessaus','Geen extras']
 Saus=['Op de frieten of Klein bakje','Groot bakje','Geen saus']
-Snacks=['Frikandel','Boulet/Mexicano/Servola','Kipnuggets/Fingers','Bicky burger','Geen snack']
+Snacks=['Frikandel/Boulet/Mexicano/Servola','Kipnuggets/Fingers','Bicky burger','Geen snack']
 Factuur=[0]
 Totaal=sum(Factuur)
 def BTW_berekenen(Totaal):
@@ -32,6 +27,41 @@ while aantal_bestellingen<Aantal_personen:
                 aantal_bestellingen=aantal_bestellingen+1
         else:
                 print('Foute invoer! Kies uit volgende opties: K,M,G',Opties)
+print('Wil je Extras?: SP/SV/G ',Extras)
+aantal_bestellingen=0
+while aantal_bestellingen<Aantal_personen:
+        print("Kies uit volgende opties: SP/SV/G",Extras)
+        keuze=input()
+        if keuze.upper()=='SP':
+                Factuur.append(1)
+                aantal_bestellingen=aantal_bestellingen+1
+        elif keuze.upper()=='SV':
+                Factuur.append(1.8)
+                aantal_bestellingen=aantal_bestellingen+1
+        elif keuze.upper()=='G':
+                Factuur.append(0)
+                aantal_bestellingen=aantal_bestellingen+1
+        else:
+                print('Foute invoer! Kies uit volgende opties: SP/SV/G',Extras)
+
+
+print('Wil je Snacks erbij?: Ja/Nee: ')
+ja_nee=input()
+if ja_nee.upper=='nee':
+        print('€', sum(Factuur), 'incl. BTW')
+elif ja_nee.upper=='ja':
+      print('Geef nu g1/g2/g3/Stop',Snacks)
+keuze=input()
+        while keuze.upper!='stop':
+                if keuze.upper() == 'g1':
+                        Factuur.append(2.3)
+                elif keuze.upper() == 'g2':
+                        Factuur.append(2.5)
+                elif keuze.upper() == 'g3':
+                        Factuur.append(3.0)
+        else:
+        print('Foute invoer! Kies uit volgende opties: g1/g2/g3/Stop', Snacks)
+else:  print('Foute invoer! Wil je Snacks erbij?: Ja/Nee: ')
 
 
 
@@ -41,9 +71,14 @@ while aantal_bestellingen<Aantal_personen:
 
 
 
-
-print('€',BTW_berekenen(sum(Factuur),'BTW')
 print('€',sum(Factuur),'incl. BTW')
+
+
+
+
+
+
+
 
 
 
